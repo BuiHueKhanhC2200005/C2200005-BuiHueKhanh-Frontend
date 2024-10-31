5ccc01833f1441fd7688d8775471f39bb20e89be
Bui Hue Khanh <khanhc2200005@studen.ctu.edu.vn>
2024-10-31 14:21:15 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page">
+    <h1>404 - Không tìm thấy trang</h1>
+    <p>
+      Oops, có vẻ như trang bạn đang tìm kiếm không tồn tại. 
+      Trở về <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
